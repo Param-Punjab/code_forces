6ef90a904e1a56acea2ee5861e31f568f8e675f7
param-punjab <mathrooparam@gmail.com>
2026-04-06 21:02:58 +0530

    another code forces string_task_118A got solved

---
+++ problems/800_rated/string_task_118A.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+string filter(string my_string) {
+  string s;
+  for (char c : my_string) {
+    if ((int)c >= 65 && (int)c <= 90) {
+      c = char((int)c + 32);
+    }
+    if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+      continue;
+    } else {
+      s.push_back(',');
+      s.push_back(c);
+    }
+  }
+  return s;
+}
+
+int main() {
+  string my_string;
+  cin >> my_string;
+  cout << filter(my_string);
+}
---
+++ problems/800_rated/watermelon_4A.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    if ((n/2)*2 == n && n > 2) cout << "YES";
+    else cout << "NO";
+}
